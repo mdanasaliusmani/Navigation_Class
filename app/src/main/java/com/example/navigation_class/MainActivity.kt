2926c4e0f6cea3090f2aebba735bf2d96084c962
Kishan Garhwal <surendrakuma123@gmail.com>
2026-01-30 02:00:32 +0530

    Nav Graph implemented

--- app/src/main/java/com/example/navigation_class/MainActivity.kt
+++ app/src/main/java/com/example/navigation_class/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +21,14 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        loadFragment(HomeFragment())
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId){
-                R.id.nav_home -> loadFragment(HomeFragment())
-                R.id.nav_profile -> loadFragment(ProfileFragment())
-            }
-            true
-        }
-    }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
 
-    fun loadFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+
+
     }
 }
